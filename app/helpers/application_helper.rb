e2f434905bf186e0c9d3d12335cb5a84cfe21c30
Grayson Wright <wright.grayson@gmail.com>
2016-09-19 19:17:01 -0700

    Format posts with Markdown
    Closes #4.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def markdown(source)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(source).html_safe
+  end
+
   def expiration_time
     if Post.any?
       Post.order(created_at: :desc).first.created_at + 7.days
